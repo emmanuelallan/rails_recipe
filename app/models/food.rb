ba9a28256a7e1ff990958d5bc681ebe532a87e89
maxthestranger <thestrangermax@gmail.com>
2022-07-26 10:20:22 +0300

    spec/

---
+++ app/models/food.rb
@@ -0,0 +1,8 @@
+class Food < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true, format: {with: /[a-zA-Z]/}
+  validates :price, presence: true, numericality: {greater_than: 0}
+  validates :quantity, presence: true, numericality: {greater_than: 0}
+end
