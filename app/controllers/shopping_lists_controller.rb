b2c19e6fe5d8f577ba88a2289f41da4dfd59aedf
selma <belhadjselma1@gmail.com>
2022-07-29 14:55:42 +0100

    Fix ruby offences

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,8 +1,7 @@
 class ShoppingListsController < ApplicationController
-    # GET /recipes or /recipes.json
-    def index
-      # @recipes = Recipe.where(public: true).order(created_at: :desc).includes(:user)
-      @recipe_food = current_user.recipe_foods.includes(:food).order(created_at: :desc)
-    end
+  # GET /recipes or /recipes.json
+  def index
+    # @recipes = Recipe.where(public: true).order(created_at: :desc).includes(:user)
+    @recipe_food = current_user.recipe_foods.includes(:food).order(created_at: :desc)
   end
-  
+end
