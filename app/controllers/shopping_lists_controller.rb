c36b7fef7a739459a17645347db26c0512b2230c
selma <belhadjselma1@gmail.com>
2022-07-29 14:54:59 +0100

    add the shopping list controller & view

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,8 @@
+class ShoppingListsController < ApplicationController
+    # GET /recipes or /recipes.json
+    def index
+      # @recipes = Recipe.where(public: true).order(created_at: :desc).includes(:user)
+      @recipe_food = current_user.recipe_foods.includes(:food).order(created_at: :desc)
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   resources :foods
   resources :public_recipes
+  resources :shopping_lists
   resources :recipes do
     resources :recipe_foods
   end
